58f93c59d3addf3d6df147cb33cf28f8de9214da
NexusNull <p.wellershaus@googlemail.com>
2018-08-31 22:06:26 +0200

    Added social buttons to the page For now just twitter github and twitch.

--- application/views/parts/page_content_end.php
+++ application/views/parts/page_content_end.php
@@ -17,13 +17,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <div class="post-navigator-title">
             </div>
             <button class="btn post-navigator-prev">
-                <i class="fa fa-angle-right"></i>
+
             </button>
         </div>
         <div class="post-navigator-index">
-
+            <i class="far fa-image"></i>
         </div>
         <div class="social-share">
+            <a href="https://twitch.tv/nexusnul">
+                <div class="social-button twitch-button" >
+                    <img src="assets/img/social_icons/twitch_glitch.svg" alt="Twitch"><span>/NexusNul</span>
+                </div>
+            </a>
+            <a href="https://twitter.com/NexusNul">
+            <div class="social-button twitter-button">
+                <img src="assets/img/social_icons/twitter.svg" alt="Twitter"><span>@NexusNul</span>
+            </div>
+            </a>
+            <a href="https://github.com/NexusNull">
+                <div class="social-button github-button">
+                    <img src="assets/img/social_icons/github.svg" alt="Github"><span>/NexusNull</span>
+                </div>
+            </a>
 
         </div>
     </div>
