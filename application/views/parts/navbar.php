c33d1a4a155d7fd92d34d391c9fca8314d7899f7
NexusNull <p.wellershaus@googlemail.com>
2018-10-30 02:36:41 +0100

    Fix navbar Home button

--- application/views/parts/navbar.php
+++ application/views/parts/navbar.php
@@ -17,13 +17,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="/">
                 <img src="/assets/img/nexus.png"/>
             </a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                <li class="active"><a href="/">Home <span class="sr-only">(current)</span></a></li>
                 <li><a href="https://github.com/NexusNull">GitHub</a></li>
                 <li><a href="#">Contact</a></li>
                 <li class="dropdown">
